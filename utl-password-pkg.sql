ed25bd1382254b154b2138dea78387a804bd4c8e
pmdba <pmdba@users.noreply.github.com>
2020-03-31 00:40:35 -0400

    Update utl-password-pkg.sql

--- utl-password-pkg.sql
+++ utl-password-pkg.sql
@@ -26,7 +26,7 @@ create or replace package body utl_password as
        my_str   varchar2 (4000);
     begin
        /* Build a random alphanumeric string of the length specified in the input */
-       for i in 1 .. p_length
+       for i in 1 .. (p_length-1)
        loop
            my_str := my_str || 
                      dbms_random.string (
@@ -37,6 +37,10 @@ create or replace package body utl_password as
        /* Replace 12 random alphanumeric characters with special characters */
        --my_str := translate (my_str, dbms_random.string ('A', 12), '_+/#)^!&*(%|');
        my_str := translate (my_str, dbms_random.string ('A', 12), '____________');
+       
+       /* Ensure that our string starts with a character */
+       my_str := dbms_random.string('a', 1) || my_str;
+
        return my_str;
     end generate_random;
 
